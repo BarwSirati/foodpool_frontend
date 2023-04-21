1aa726e40289383611a5cab79a7f34c7e48b7e85
BarwSirati <sirati10546@gmail.com>
2023-04-21 22:13:18 +0700

    chore: add colorful style

--- src/Order/Order.jsx
+++ src/Order/Order.jsx
@@ -5,18 +5,6 @@ import OrderList from './OrdeList'
 import Container from '../components/Container'
 
 const Order = () => {
-  // useEffect(() => {
-  //     try {
-  //         axios.get(url)
-  //             .then((res) => {
-  //                 setOrder(res)
-  //                 console.log(res)
-  //             })
-  //     } catch (error) {
-  //         console.log(error)
-  //     }
-  // }, [])
-
   return (
     <Container>
       <h2 className="text-2xl font-semibold">Order</h2>
