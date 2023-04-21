d48d043dfcf198fba1937327e9a3183a74c4e90e
BarwSirati <sirati10546@gmail.com>
2023-04-22 00:14:13 +0700

    feat: Loading

--- src/components/ProtectRoute.jsx
+++ src/components/ProtectRoute.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { useAuth } from '../contexts/AuthContext'
 import { Navigate } from 'react-router-dom'
+import Loading from './Loading'
 
 const ProtectRoute = ({ children }) => {
   const { isAuthenticated, isLogged, isLoading } = useAuth()
   if (isLoading) {
-    return <div>Loading</div>
+    return <Loading />
   }
 
   if (!isAuthenticated && !isLogged) {
