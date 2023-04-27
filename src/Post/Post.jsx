99197f5e5f4fc830a7211dc8535592f8902a2791
Teerawat Chanvipardilok <82800595+Teerawat36167@users.noreply.github.com>
2023-04-28 00:19:16 +0700

    Chore : return PostList

--- src/Post/Post.jsx
+++ src/Post/Post.jsx
@@ -1,32 +1,43 @@
-import React from 'react'
-import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
+import { useAuth } from '../contexts/AuthContext'
 import PostList from './PostList'
 import Container from '../components/Container'
 
-const Post = () => {
-    
-    // useEffect(() => {
-    //     try {
-    //         axios.get(url)
-    //             .then((res) => {
-    //                 setOrder(res)
-    //                 console.log(res)
-    //             })
-    //     } catch (error) {
-    //         console.log(error)
-    //     }
-    // }, [])
+import { getPostByUserId } from '../services/post.service'
 
-    return (
-        <Container>
-            <h2 className="text-2xl font-semibold">My post history</h2>
-            <div className="pt-5 space-y-4">
+const Post = () => {
+  const [isloading, setIsLoading] = useState(false)
+  const [postData, setPostData] = useState([])
+
+  const { user } = useAuth()
+
+  useEffect(() => {
+    const getPostByUser = async () => {
+      setIsLoading(true)
+      const res = await getPostByUserId(user.id)
+      setPostData(res)
+      setIsLoading(false)
+    }
+    getPostByUser()
+  }, [])
+
+  return (
+    <Container>
+      <h2 className="text-2xl font-semibold">My post history</h2>
+      <div className="pt-5 space-y-4">
+        {isloading ? (
+          <h1 className="text-3xl font-semibold">Loading</h1>
+        ) : (
+          postData.map((data) => {
+            return (
                 <PostList />
-                <PostList />
-            </div>
-        </Container>
-    )
+            )
+          })
+        )}
+      </div>
+    </Container>
+  )
 }
 
 export default Post
