6261fb00477b7b482018df66daa13053a4164378
BarwSirati <sirati10546@gmail.com>
2023-04-29 22:01:58 +0700

    chore: footer

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,7 @@ const App = () => {
 
         <Route path="*" element={<Error />} />
       </Routes>
-      <Footer />
+      {isAuthenticated && isLogged && !isLoading && <Footer />}
     </Layout>
   )
 }
