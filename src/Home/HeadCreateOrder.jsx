47133dedf110644a78ae3d0446639af70cdf899f
Sobitora <web_124@hotmail.com>
2023-05-03 22:48:40 +0700

    chore: UI createmenu

--- src/Home/HeadCreateOrder.jsx
+++ src/Home/HeadCreateOrder.jsx
@@ -4,7 +4,7 @@ const HeadCreateOrder = ({post}) =>{
 
     return(
         <h2 className="text-2xl mb-5">
-        <div className="w-full rounded-lg text-lg bg-blue-400 px-5 py-2 shadow-lg">
+        <div className="w-full rounded-lg text-lg bg-blue-100 px-5 py-2 shadow-lg">
           <h1 className="md:text-2xl text-lg text-center mb-2">
             {post.menuName} ({post.stall.name})
           </h1>
